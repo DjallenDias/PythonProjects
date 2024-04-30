6b98675d0bf2a54b2c28dac29701c4eef9538ff4
Djállen Fabrício <djallendias@gmail.com>
2024-04-30 11:29:09 -0300

    added a thread in both files

--- Sockets/client.py
+++ Sockets/client.py
@@ -3,6 +3,12 @@ import socket
 import threading
 import argparse
 
+def recieve(sock):
+    while thread_run:
+        data = sock.recv(1024).decode()
+        if data == "": break
+        print(data)
+
 parser = argparse.ArgumentParser(description="Chat Server side socket")
 parser.add_argument("HOST", help="Host or Address to server run on")
 parser.add_argument("PORT", help="Port for the server to check for connections", type=int)
@@ -10,11 +16,26 @@ parser.add_argument("--username")
 
 args = parser.parse_args()
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((args.HOST, args.PORT))
-
 if (args.username):
     username = args.username
 else:
     username = "guest"
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect((args.HOST, args.PORT))
+
+thread_run = True
+
+recieve_thread = threading.Thread(target=recieve, args=[sock])
+recieve_thread.start()
+
+while True:
+    msg = input()
+    if not msg:
+        thread_run = False
+        sock.shutdown(socket.SHUT_RDWR)
+        break
+
+    sock.send(msg.encode())
+
+sock.close()
--- Sockets/server.py
+++ Sockets/server.py
@@ -3,6 +3,13 @@ import socket
 import threading
 import argparse
 
+def recieve(conn):
+    while thread_run:
+        data = conn.recv(1024).decode()
+        if not data: break
+        print(data)
+
+
 parser = argparse.ArgumentParser(description="Chat Server side socket")
 parser.add_argument("HOST", help="Host or Address to server run on")
 parser.add_argument("PORT", help="Port for the server to check for connections", type=int)
@@ -10,13 +17,28 @@ parser.add_argument("--username")
 
 args = parser.parse_args()
 
+if (args.username):
+    username = args.username
+else:
+    username = "guest"
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind((args.HOST, args.PORT))
 sock.listen()
 
 conn, addr = sock.accept()
 
-if (args.username):
-    username = args.username
-else:
-    username = "guest"
+thread_run = True
+
+recieve_thread = threading.Thread(target=recieve, args=[conn])
+recieve_thread.start()
+
+while True:
+    msg = input()
+    if not msg: 
+        thread_run = False
+        conn.close()
+        break
+    conn.send(msg.encode())
+
+sock.close()
