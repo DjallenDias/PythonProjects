fd6a28489300f3ab52d1408b6ddf69b731d81226
Djállen Fabrício <djallendias@gmail.com>
2024-04-30 10:15:48 -0300

    added CLI config

--- Sockets/client.py
+++ Sockets/client.py
@@ -1,14 +1,20 @@
 import sys
 import socket
 import threading
+import argparse
 
-HOST = "localhost"
-PORT = 8080
+parser = argparse.ArgumentParser(description="Chat Server side socket")
+parser.add_argument("HOST", help="Host or Address to server run on")
+parser.add_argument("PORT", help="Port for the server to check for connections", type=int)
+parser.add_argument("--username")
+
+args = parser.parse_args()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
+sock.connect((args.HOST, args.PORT))
 
-
-data = sock.recv(1024)
-print(data.decode())
+if (args.username):
+    username = args.username
+else:
+    username = "guest"
 
--- Sockets/server.py
+++ Sockets/server.py
@@ -1,16 +1,22 @@
 import sys
 import socket
 import threading
+import argparse
 
-HOST = "localhost"
-PORT = 8080
+parser = argparse.ArgumentParser(description="Chat Server side socket")
+parser.add_argument("HOST", help="Host or Address to server run on")
+parser.add_argument("PORT", help="Port for the server to check for connections", type=int)
+parser.add_argument("--username")
+
+args = parser.parse_args()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((HOST, PORT))
+sock.bind((args.HOST, args.PORT))
 sock.listen()
 
 conn, addr = sock.accept()
 
-print(f"{addr} connected")
-
-conn.send("hey".encode())
+if (args.username):
+    username = args.username
+else:
+    username = "guest"
