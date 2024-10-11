a31876de9a319217607198def1f5c03a51c2298e
Djállen Fabrício <djallendias@gmail.com>
2024-10-11 19:08:29 -0300

    /users/me urlroutes added

--- FastAPI/urlroutes.py
+++ FastAPI/urlroutes.py
@@ -5,3 +5,11 @@ app = FastAPI()
 @app.get("/items/{item_id}")
 async def get_item(item_id: str):
     return {"item_id": item_id}
+
+@app.get("/users/me")
+async def get_user_me():
+    return {"user_id": "the current user"}
+
+@app.get("/users/{user_id}")
+async def get_user(user_id: str):
+    return {"user_id": user_id}
