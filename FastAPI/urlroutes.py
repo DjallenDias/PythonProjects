d99638dec3db5fa60725babff02f60b0a6b4ddaa
Djállen Fabrício <djallendias@gmail.com>
2024-10-11 19:11:36 -0300

    urlroutes model name route added

--- FastAPI/urlroutes.py
+++ FastAPI/urlroutes.py
@@ -1,10 +1,17 @@
 from fastapi import FastAPI
+from enum import Enum
+
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
 
 app = FastAPI()
 
 @app.get("/items/{item_id}")
 async def get_item(item_id: str):
     return {"item_id": item_id}
+
 
 @app.get("/users/me")
 async def get_user_me():
@@ -13,3 +20,16 @@ async def get_user_me():
 @app.get("/users/{user_id}")
 async def get_user(user_id: str):
     return {"user_id": user_id}
+
+
+@app.get("/models/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name is ModelName.alexnet:
+        return {"model_name": model_name,
+                "message": "Deep Learning FTW!"}
+    
+    if model_name.value == "lenet":
+        return {"model_name": model_name,
+                "message": "LeCNN all the images"}
+    
+    return {"model_name": model_name, "message": "Have some residuals"}
